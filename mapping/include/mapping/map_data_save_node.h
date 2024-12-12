4728a1d775141acd8dc277251f9ea1df046f659a
Vinicius <vinicius.vidal@engenharia.ufjf.br>
2024-12-12 16:42:20 -0300

    updating mapping to work with vehicle extrinsics

--- mapping/include/mapping/map_data_save_node.h
+++ mapping/include/mapping/map_data_save_node.h
@@ -37,23 +37,23 @@ class MapDataSaver
 {
 public:
     /// @brief Constructor
-    MapDataSaver(ros::NodeHandle& nh);
+    MapDataSaver(ros::NodeHandle &nh);
 
 private:
     /// @brief The mapping callback
     /// @param pointcloud_msg The point cloud message
     /// @param gps_msg The GPS message
     /// @param odom_msg The odometry message
-    void mappingCallback(const sensor_msgs::PointCloud2::ConstPtr& pointcloud_msg,
-                  const sensor_msgs::NavSatFix::ConstPtr& gps_msg,
-                  const nav_msgs::Odometry::ConstPtr& odom_msg);
+    void mappingCallback(const sensor_msgs::PointCloud2::ConstPtr &pointcloud_msg,
+                         const sensor_msgs::NavSatFix::ConstPtr &gps_msg,
+                         const nav_msgs::Odometry::ConstPtr &odom_msg);
 
     /// @brief Callback to run at node kill
     void onShutdown();
 
     /// @brief Callback to get the yaw angle from compass
     /// @param msg The compass message
-    void compassCallback(const std_msgs::Float64::ConstPtr& msg);
+    void compassCallback(const std_msgs::Float64::ConstPtr &msg);
 
     /// @brief The synchronization policy
     using SyncPolicy = message_filters::sync_policies::ApproximateTime<
@@ -65,7 +65,7 @@ private:
     ros::Subscriber compass_subscription_;
     message_filters::Subscriber<sensor_msgs::PointCloud2> pointcloud_sub_;
     message_filters::Subscriber<sensor_msgs::NavSatFix> gps_sub_;
-    message_filters::Subscriber<nav_msgs::Odometry> odom_sub_;  
+    message_filters::Subscriber<nav_msgs::Odometry> odom_sub_;
     std::shared_ptr<message_filters::Synchronizer<SyncPolicy>> sync_;
 
     /// @brief Paths
@@ -73,19 +73,25 @@ private:
     std::string odometry_file_path_;
     std::string gps_imu_poses_file_path_;
 
-    ///@brief Point cloud to save the map in tiles
+    /// @brief Point cloud to save the map in tiles
     int cloud_counter_{0};
     int cloud_save_interval_{10};
     int min_counter_to_account_for_velocity_{100};
     float min_velocity_to_count_as_movement_{0.1f}; // [m/s]
     pcl::PointCloud<PointT>::Ptr cloud_map_frame_;
-    nav_msgs::Odometry last_odom_;
+
+    /// @brief previous odometry message data
+    Eigen::Matrix4f last_odom_{Eigen::Matrix4f::Identity()};
+    ros::Time last_odom_time_, current_odom_time_;
 
     /// @brief Yaw angle from compass
     double current_compass_yaw_{0.0}; // -M_PI to M_PI [RAD]
 
     /// @brief Debug flag
     bool debug_{false};
+
+    /// @brief Vehicle extrinsics, vehicle with respect to lidar
+    Eigen::Matrix4f lidar_T_vehicle_{Eigen::Matrix4f::Identity()};
 };
 
-#endif  // MAP_DATA_SAVE_NODE_H
+#endif // MAP_DATA_SAVE_NODE_H
--- mapping/src/map_data_save_node.cpp
+++ mapping/src/map_data_save_node.cpp
@@ -1,6 +1,6 @@
 #include "mapping/map_data_save_node.h"
 
-MapDataSaver::MapDataSaver(ros::NodeHandle& nh)
+MapDataSaver::MapDataSaver(ros::NodeHandle &nh)
 {
     // Parameters
     ros::NodeHandle pnh("~");
@@ -11,13 +11,33 @@ MapDataSaver::MapDataSaver(ros::NodeHandle& nh)
     pnh.param("/mapping/min_counter_to_account_for_velocity", min_counter_to_account_for_velocity_, 100);
     pnh.param("/mapping/min_velocity_to_count_as_movement", min_velocity_to_count_as_movement_, 0.1f);
 
+    // Vehicle extrinsics and box region filter params
+    float vehicle_x_lidar, vehicle_y_lidar, vehicle_z_lidar;
+    float vehicle_roll_lidar, vehicle_pitch_lidar, vehicle_yaw_lidar;
+    pnh.param("/lidar_extrinsics/x_lidar_vehicle", vehicle_x_lidar, 0.0f);
+    pnh.param("/lidar_extrinsics/y_lidar_vehicle", vehicle_y_lidar, 0.0f);
+    pnh.param("/lidar_extrinsics/z_lidar_vehicle", vehicle_z_lidar, 0.0f);
+    pnh.param("/lidar_extrinsics/roll_lidar_vehicle", vehicle_roll_lidar, 0.0f);
+    pnh.param("/lidar_extrinsics/pitch_lidar_vehicle", vehicle_pitch_lidar, 0.0f);
+    pnh.param("/lidar_extrinsics/yaw_lidar_vehicle", vehicle_yaw_lidar, 0.0f);
+    Eigen::Matrix4f vehicle_T_lidar = Eigen::Matrix4f::Identity();
+    Eigen::Matrix3f vehicle_R_lidar;
+    vehicle_R_lidar = Eigen::AngleAxisf(M_PI/180.0f*vehicle_roll_lidar, Eigen::Vector3f::UnitX()) *
+                      Eigen::AngleAxisf(M_PI/180.0f*vehicle_pitch_lidar, Eigen::Vector3f::UnitY()) *
+                      Eigen::AngleAxisf(M_PI/180.0f*vehicle_yaw_lidar, Eigen::Vector3f::UnitZ());
+    vehicle_T_lidar.block<3, 3>(0, 0) = vehicle_R_lidar;
+    vehicle_T_lidar(0, 3) = vehicle_x_lidar;
+    vehicle_T_lidar(1, 3) = vehicle_y_lidar;
+    vehicle_T_lidar(2, 3) = vehicle_z_lidar;
+    lidar_T_vehicle_ = vehicle_T_lidar.inverse();
+
     // Create a folder, making sure it does not exist before
     // If it exists, delete it and create it again
     folder_save_path_ = std::string(std::getenv("HOME")) + "/" + folder_save_path_;
     if (FileManipulation::directoryExists(folder_save_path_))
     {
         std::string command = "rm -rf " + folder_save_path_ + "/*";
-        if(!system(command.c_str()))
+        if (!system(command.c_str()))
         {
             ROS_WARN("Reseting folder %s to store new mapping data!", folder_save_path_.c_str());
         }
@@ -42,15 +62,20 @@ MapDataSaver::MapDataSaver(ros::NodeHandle& nh)
         &MapDataSaver::compassCallback, this);
 
     // Initialize synchronized subscribers
-    pointcloud_sub_.subscribe(nh, "/cloud_registered", 10);
+    pointcloud_sub_.subscribe(nh, "/cloud_registered_body", 10);
     gps_sub_.subscribe(nh, "/mavros/global_position/global", 10);
     odom_sub_.subscribe(nh, "/Odometry", 10);
     sync_.reset(new message_filters::Synchronizer<SyncPolicy>(
         SyncPolicy(50), pointcloud_sub_, gps_sub_, odom_sub_));
     sync_->registerCallback(boost::bind(&MapDataSaver::mappingCallback, this, _1, _2, _3));
+
+    // Initialize the last odometry message
+    last_odom_ = Eigen::Matrix4f::Identity();
+    last_odom_time_ = ros::Time::now();
+    current_odom_time_ = ros::Time::now();
 }
 
-void MapDataSaver::compassCallback(const std_msgs::Float64::ConstPtr& msg)
+void MapDataSaver::compassCallback(const std_msgs::Float64::ConstPtr &msg)
 {
     // Invert the yaw based on Ardupilot convention that clockwise is positive
     current_compass_yaw_ = (90.0 - msg->data) * M_PI / 180.0;
@@ -65,26 +90,38 @@ void MapDataSaver::compassCallback(const std_msgs::Float64::ConstPtr& msg)
     }
 }
 
-void MapDataSaver::mappingCallback(const sensor_msgs::PointCloud2::ConstPtr& pointcloud_msg,
-                const sensor_msgs::NavSatFix::ConstPtr& gps_msg,
-                const nav_msgs::Odometry::ConstPtr& odom_msg)
+void MapDataSaver::mappingCallback(const sensor_msgs::PointCloud2::ConstPtr &pointcloud_msg,
+                                   const sensor_msgs::NavSatFix::ConstPtr &gps_msg,
+                                   const nav_msgs::Odometry::ConstPtr &odom_msg)
 {
-    // Add the point cloud to the map
+    // Convert the odometry message to a homogeneous matrix
+    Eigen::Matrix4f odom_T_lidar = Eigen::Matrix4f::Identity();
+    Eigen::Quaternionf odom_q_lidar(odom_msg->pose.pose.orientation.w,
+                                    odom_msg->pose.pose.orientation.x,
+                                    odom_msg->pose.pose.orientation.y,
+                                    odom_msg->pose.pose.orientation.z);
+    Eigen::Vector3f odom_t_lidar(odom_msg->pose.pose.position.x, odom_msg->pose.pose.position.y, odom_msg->pose.pose.position.z);
+    odom_T_lidar.block<3, 3>(0, 0) = odom_q_lidar.toRotationMatrix();
+    odom_T_lidar.block<3, 1>(0, 3) = odom_t_lidar;
+    // Apply the extrinsics to the sensor frame
+    Eigen::Matrix4f odom_T_vehicle = Eigen::Matrix4f::Identity();
+    odom_T_vehicle = lidar_T_vehicle_* odom_T_lidar;
+
+    // Transform and add the point cloud to the map
     pcl::PointCloud<PointT>::Ptr cloud = pcl::PointCloud<PointT>::Ptr(new pcl::PointCloud<PointT>);
     pcl::fromROSMsg(*pointcloud_msg, *cloud);
+    pcl::transformPointCloud(*cloud, *cloud, odom_T_vehicle);
     *cloud_map_frame_ += *cloud;
     ++cloud_counter_;
 
     // Calculate velocity
-    const Eigen::Vector3f current_position(odom_msg->pose.pose.position.x,
-                                           odom_msg->pose.pose.position.y,
-                                           odom_msg->pose.pose.position.z);
-    const Eigen::Vector3f previous_position(last_odom_.pose.pose.position.x,
-                                            last_odom_.pose.pose.position.y,
-                                            last_odom_.pose.pose.position.z);
-    const float dt = (odom_msg->header.stamp - last_odom_.header.stamp).toSec();
+    const Eigen::Vector3f current_position(odom_T_vehicle.block<3, 1>(0, 3));
+    const Eigen::Vector3f previous_position(last_odom_.block<3, 1>(0, 3));
+    current_odom_time_ = odom_msg->header.stamp;
+    const float dt = (current_odom_time_ - last_odom_time_).toSec();
     const float velocity = (current_position - previous_position).norm() / dt;
-    last_odom_ = *odom_msg;
+    last_odom_ = odom_T_vehicle;
+    last_odom_time_ = current_odom_time_;
 
     // Do not save if conditions are not met
     if (cloud_counter_ > min_counter_to_account_for_velocity_ &&
@@ -109,19 +146,19 @@ void MapDataSaver::mappingCallback(const sensor_msgs::PointCloud2::ConstPtr& poi
     // Write a line to the odometry file
     // It should be tx ty tz
     std::ofstream odom_positions_file(odometry_file_path_, std::ios::app);
-    odom_positions_file << odom_msg->pose.pose.position.x << " "
-                << odom_msg->pose.pose.position.y << " "
-                << odom_msg->pose.pose.position.z << std::endl;
+    odom_positions_file << current_position.x() << " "
+                        << current_position.y() << " "
+                        << current_position.z() << std::endl;
     odom_positions_file.close();
 
     // Write a line to the GPS IMU file
     // It should be lat lon alt yaw
     std::ofstream gps_imu_poses_file(gps_imu_poses_file_path_, std::ios::app);
     gps_imu_poses_file << std::fixed << std::setprecision(8)
-                        << gps_msg->latitude << " "
-                        << gps_msg->longitude << " "
-                        << gps_msg->altitude << " "
-                        << current_compass_yaw_ << std::endl;
+                       << gps_msg->latitude << " "
+                       << gps_msg->longitude << " "
+                       << gps_msg->altitude << " "
+                       << current_compass_yaw_ << std::endl;
     gps_imu_poses_file.close();
 }
 
